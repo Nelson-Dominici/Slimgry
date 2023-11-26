8d5fdc6c665a25f07b22b0a57b86c7743a86381c
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-11-26 17:03:55 -0300

    Adds 0 and false as acceptable values.

--- src/Validation/Methods/RequiredMethod.php
+++ src/Validation/Methods/RequiredMethod.php
@@ -7,11 +7,18 @@ namespace NelsonDominici\Slimgry\Validation\Methods;
 class RequiredMethod extends MethodHelper
 {
     public function __invoke(): void
-    {
-        $exceptionMessage = 'The field '.$this->fieldName.' is required';
-        
-        $expression = empty($this->requestBody[$this->fieldName]);
-        
-        $this->assertAndThrow($expression , $exceptionMessage, 400);
+    {    
+        if (
+            array_key_exists($this->fieldName, $this->requestBody) &&
+            (
+                $this->requestBody[$this->fieldName] ||
+                $this->requestBody[$this->fieldName] === false ||
+                $this->requestBody[$this->fieldName] === 0
+            )
+        ) {
+            return;
+        }
+    
+        $this->throwException('The field '.$this->fieldName.' is required');
     }
 }
