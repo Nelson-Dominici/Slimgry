3b805ec1dc64f04bd09d374b13df9c7316069d07
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-11-28 22:50:11 -0300

    Adding custom exception messages.

--- src/Slimgry.php
+++ src/Slimgry.php
@@ -1,24 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NelsonDominici\Slimgry;
 
 use Psr\Http\Message\{ 
 	ResponseInterface as Response,
 	ServerRequestInterface as Request
 };
-
+use NelsonDominici\Slimgry\Validation\Validation;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-class Slimgry extends Validator
+final class Slimgry extends Validation
 {
 	public function __construct(
-		private array $bodyValidations
+		private array $bodyValidations,
+		private array $customExceptionMessages = []
 	){}
-
+	
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
-	    $this->validate($this->bodyValidations, $request->getParsedBody());
-
+	    $this->validate(
+            $this->bodyValidations, 
+            $request->getParsedBody(), 
+            $this->customExceptionMessages
+        );
+        
 	  	return $handler->handle($request);
     }
 }
