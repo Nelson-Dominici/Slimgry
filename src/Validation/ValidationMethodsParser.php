a20c18b5e3422315199064e9568eca23e67687cd
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-11-30 20:43:51 -0300

    Improving method and variable names for better readability.

--- src/Validation/ValidationMethodsParser.php
+++ src/Validation/ValidationMethodsParser.php
@@ -6,37 +6,37 @@ namespace NelsonDominici\Slimgry\Validation;
 
  trait ValidationMethodsParser
 {
-    protected function getParserdValidations(string $fieldValidations): array
+    protected function getParsedValidationMethods(string $fieldValidationMethods): array
     {
-        $this->checkFieldValidations($fieldValidations);
+        $this->checkFieldValidationMethods($fieldValidationMethods);
         
-        return $this->getUniqueFieldValidations($fieldValidations);
+        return $this->getUniqueValidationMethods($fieldValidationMethods);
     }
     
-	private function checkFieldValidations(string $fieldValidations): void
+	private function checkFieldValidationMethods(string $fieldValidationMethods): void
 	{        
-        if (!is_string($fieldValidations) || $fieldValidations === '') {
+        if (!is_string($fieldValidationMethods) || $fieldValidationMethods === '') {
             throw new \Exception('Slimgry validations must be a string.' , 422);                      
         }
         
         $pattern = '/[^|]*:[^|]*:[^|]*/'; //Checking if there is more than one (:) in a validation method.
 
-        if (preg_match($pattern, $fieldValidations)) {
+        if (preg_match($pattern, $fieldValidationMethods)) {
             throw new \Exception('Invalid validation method format. Use only one colon (:).', 422);
         }
     }
 
-	private function getUniqueFieldValidations(string $fieldValidations): array
+	private function getUniqueValidationMethods(string $fieldValidationMethods): array
 	{
-        $uniqueValidations = [];
+        $uniqueValidationMethods = [];
         
-        foreach (explode('|', $fieldValidations) as $fieldValidation) {
+        foreach (explode('|', $fieldValidationMethods) as $fieldValidationMethod) {
         
-            $validationMethod = explode(':', $fieldValidation)[0];
+            $validationMethod = explode(':', $fieldValidationMethod)[0];
 
-            $uniqueValidations[$validationMethod] = $fieldValidation;
+            $uniqueValidationMethods[$validationMethod] = $fieldValidationMethod;
         }
 
-        return array_values($uniqueValidations);
+        return array_values($uniqueValidationMethods);
 	}
 }
