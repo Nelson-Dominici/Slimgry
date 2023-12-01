93fa6e049cbd8cb8cef2cbe668303618327d340f
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-11-30 23:02:51 -0300

    Implementing a new instantiation method for validation methods with custom messages.

--- src/Validation/ValidationMethods.php
+++ src/Validation/ValidationMethods.php
@@ -7,7 +7,10 @@ namespace NelsonDominici\Slimgry\Validation;
 abstract class ValidationMethods
 {
     protected const METHODS = [
+        'min' => Methods\MinMethod::class,
+        'trim' => Methods\TrimMethod::class,
         'max' => Methods\MaxMethod::class,
+        'string' => Methods\StringMethod::class,
         'required' => Methods\RequiredMethod::class
     ];
 
@@ -16,5 +19,34 @@ abstract class ValidationMethods
         if (!array_key_exists($validationMethod, self::METHODS)) {
             throw new \Exception("Validation method '$validationMethod' does not exist.", 422);
         }    
+    }
+
+    protected function getValidationMethodInstance(string $fieldName, array $requestBody, array $validationMethodParts, array $customExceptionMessages): object
+    {
+        $validationMethodPath = self::METHODS[$validationMethodParts[0]];
+
+        $customExceptionMessage = $this->customExceptionMessage(
+            $fieldName, 
+            $validationMethodParts[0], 
+            $customExceptionMessages
+        );
+        
+        return new $validationMethodPath(
+            $fieldName,
+            $requestBody,
+            $validationMethodParts, 
+            $customExceptionMessage
+        );        
+    }
+    
+    private function customExceptionMessage(string $fieldName, string $validationMethod, array $customExceptionMessages): string
+    {
+        $customExceptionMessageField = $fieldName.'.'.$validationMethod;
+        
+        if (!array_key_exists($customExceptionMessageField, $customExceptionMessages)) {
+            return '';
+        }
+        
+        return $customExceptionMessages[$customExceptionMessageField];
     }
 }
