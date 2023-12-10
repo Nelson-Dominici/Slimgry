7fc3f8ef8fc670a4460a62f3b1454e4d9b678efb
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-12-09 23:26:39 -0300

    Using the customExceptionMessageProvider composition.

--- src/ValidationMethod/ValidationMethodInstantiator.php
+++ src/ValidationMethod/ValidationMethodInstantiator.php
@@ -7,20 +7,29 @@ namespace NelsonDominici\Slimgry\ValidationMethod;
 class ValidationMethodInstantiator
 {    
     private ValidationMethodFinder $methodFinder;
-    
+    private CustomExceptionMessageProvider $messageProvider;
+
     public function __construct()
     {
         $this->methodFinder = new ValidationMethodFinder();
+        $this->messageProvider = new CustomExceptionMessageProvider();
     }
 
     public function getInstance(
+        string $fieldToValidate,
         string $validationMethod,
-        string $customExceptionMessage 
+        array $customExceptionMessages
     ): Methods\ValidationMethod 
     {
         $validationMethodParts = explode(':', $validationMethod);
     
         $validationMethodName = $validationMethodParts[0];
+    
+        $customExceptionMessage = $this->messageProvider->getCustomMessage(
+            $fieldToValidate,
+            $validationMethod,
+            $customExceptionMessages
+        );
         
         $validationMethodPath = $this->methodFinder->find($validationMethodName);
 
