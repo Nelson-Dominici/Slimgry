61c770ac4b13bbea906a6f17b76599db83d92496
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-01-18 19:04:33 -0300

    Introducing validation method syntax exception

---
+++ src/Exceptions/ValidationMethodSyntaxException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NelsonDominici\Slimgry\Exceptions;
+
+class ValidationMethodSyntaxException extends \InvalidArgumentException
+{
+    public function __construct(
+        string $message,
+        private string $validationMethod
+    ) {
+        parent::__construct('Invalid validation method sintax. '.$message, 422);
+    }
+
+    function getValidationMethod()
+    {
+        return $this->validationMethod;
+    }
+} 
