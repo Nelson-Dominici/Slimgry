a5cbe6ddd3edfca59bc799d6e8eaa345f7bdaf92
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-01-19 11:33:31 -0300

    Throwing new exception if the validation method does not exist

--- src/ValidationMethod/ValidationMethodFinder.php
+++ src/ValidationMethod/ValidationMethodFinder.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 
 namespace NelsonDominici\Slimgry\ValidationMethod;
 
+use NelsonDominici\Slimgry\Exceptions\ValidationMethodSyntaxException;
+
 class ValidationMethodFinder
 {
     public function find(string $validationMethodName): string
     {
         $validationMethodPath = __NAMESPACE__.'\Methods\\'.ucfirst($validationMethodName)."Method";
         
-        if (class_exists($validationMethodPath)) {
-            return $validationMethodPath;
+        if (!class_exists($validationMethodPath) || $validationMethodName === 'validation') {
+
+            $message = "Validation method \"$validationMethodName\" does not exist.";
+
+            throw new ValidationMethodSyntaxException($message, $validationMethodName, 404);
         } 
-        
-        throw new \Exception(
-            "Validation method '$validationMethodName' does not exist.", 422
-        );
+
+        return $validationMethodPath;
     }
 }
