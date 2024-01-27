38476405766c6fd18896d4fbdc280d28b165dee8
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-01-26 23:18:07 -0300

    Introducing ValidationMethodInstantiator tests

---
+++ tests/ValidationMethod/ValidationMethodInstantiatorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\ValidationMethod;
+
+use PHPUnit\Framework\TestCase;
+
+use NelsonDominici\Slimgry\ValidationMethod\{
+    ValidationMethodFinder,
+    ValidationMethodInstantiator,
+    CustomExceptionMessageProvider
+};
+
+use NelsonDominici\Slimgry\ValidationMethod\Methods\{
+    StringMethod,
+    ValidationMethod
+};
+
+class ValidationMethodInstantiatorTest extends TestCase
+{    
+    function testMustReturnTheInstanceOfAValidationMethod(): void
+    {
+        $exampleFieldToValidate = 'name';
+        $exampleValidationMethod = 'string';
+        
+        $validationMethodFinder = $this->createMock(
+            ValidationMethodFinder::class
+        );
+
+        $customExceptionMessageProvider = $this->createMock(
+            CustomExceptionMessageProvider::class
+        );
+
+        $validationMethodFinder
+            ->method('find')
+            ->willReturn(StringMethod::class);
+
+        $customExceptionMessageProvider
+            ->method('getCustomMessage')
+            ->willReturn('');
+
+        $validationMethodInstantiator = new ValidationMethodInstantiator(
+            $validationMethodFinder,
+            $customExceptionMessageProvider
+        );
+
+        $validationMethodInstance = $validationMethodInstantiator->getInstance(
+            $exampleFieldToValidate, $exampleValidationMethod
+        );
+
+        $this->assertInstanceOf(ValidationMethod::class, $validationMethodInstance);
+    }
+}
