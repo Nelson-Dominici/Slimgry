bbe70d4feb8be711c82a34ce049e4609e9149b95
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-02-01 19:28:17 -0300

    Introducing the tests of the ValidationMethod abstract class

---
+++ tests/ValidationMethod/Methods/ValidationMethodTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\ValidationMethod\Methods;
+
+use PHPUnit\Framework\TestCase;
+use NelsonDominici\Slimgry\Exceptions\ValidationMethodException;
+use NelsonDominici\Slimgry\ValidationMethod\Methods\ValidationMethod;
+use NelsonDominici\Slimgry\Exceptions\ValidationMethodSyntaxException;
+
+class ValidationMethodTest extends TestCase
+{   
+    private ValidationMethod $validationMethodMock;
+    private \ReflectionClass $validationMethodReflection;
+
+    public function setUp(): void
+    {
+        $this->validationMethodMock = $this
+            ->getMockBuilder(ValidationMethod::class)
+            ->setConstructorArgs(['name', ['max','test'], ''])
+            ->getMockForAbstractClass();
+
+        $this->validationMethodReflection = new \ReflectionClass(
+            ValidationMethod::class
+        );
+    } 
+
+    public function testThrowsExceptionForNonNumericKeyValueValidationMethodValue(): void
+    {
+        $getNumericValueMethod = $this->validationMethodReflection->getMethod(
+            'getNumericValue'
+        );
+
+        $getNumericValueMethod->setAccessible(true);
+        
+        $this->expectException(ValidationMethodSyntaxException::class);
+    
+        $this->expectExceptionMessage(
+            "The validation method \"max:test\" does not have a numeric value."
+        );
+    
+        $this->expectExceptionCode(500);
+
+        $getNumericValueMethod->invoke($this->validationMethodMock);    
+    }
+
+    public function testAssertAndThrowMethodThrowsExceptionWithCustomMessageIfExpressionIsTrue()
+    {
+        $customMessage = 'This custom message must appear.';
+        
+        $assertAndThrowMethod = $this->validationMethodReflection->getMethod(
+            'assertAndThrow'
+        );
+
+        $assertAndThrowMethod->setAccessible(true);
+
+        $this->expectException(ValidationMethodException::class);
+    
+        $this->expectExceptionMessage($customMessage);
+    
+        $this->expectExceptionCode(422);
+
+       $assertAndThrowMethod->invoke($this->validationMethodMock, true, $customMessage);
+    }
+
+    public function testThrowExceptionMethodThrowsExceptionWithCustomMessage(): void
+    {
+        $customMessage = 'This custom message must appear.';
+
+        $throwExceptionMethod = $this->validationMethodReflection->getMethod(
+            'throwException'
+        );
+
+        $throwExceptionMethod->setAccessible(true);
+
+        $this->expectException(ValidationMethodException::class);
+    
+        $this->expectExceptionMessage($customMessage);
+    
+        $this->expectExceptionCode(422);
+
+       $throwExceptionMethod->invoke($this->validationMethodMock, $customMessage);
+    }
+}
