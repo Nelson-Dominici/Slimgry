a8cbfbeac99dcb06b123137c1d97305a0644ef2a
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-02-02 19:53:01 -0300

    Introducing the StringMethod class tests

---
+++ tests/ValidationMethod/Methods/StringMethodTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\ValidationMethod\Methods;
+
+use PHPUnit\Framework\TestCase;
+use NelsonDominici\Slimgry\Exceptions\ValidationMethodException;
+use NelsonDominici\Slimgry\ValidationMethod\Methods\StringMethod;
+
+class StringMethodTest extends TestCase
+{
+    private StringMethod $stringMethod;
+    
+    public function setUp(): void
+    {
+        $fieldToValidate = 'name';
+        $validationParts = ['string'];
+        $customExceptionMessage = '';
+
+        $this->stringMethod = new StringMethod(
+            $fieldToValidate, 
+            $validationParts, 
+            $customExceptionMessage
+        );
+    }
+
+    public function testExecuteMethodReturnsNullIfFieldToValidateDoesNotExist(): void
+    {
+        $this->assertNull(
+            $this->stringMethod->execute(['ThisFieldDoesNotExist' => 'Nelson'])
+        );
+    }
+
+    public function testExecuteMethodThrowsExceptionIfFieldToValidateIsNotAString(): void
+    {
+        $this->expectException(ValidationMethodException::class);
+        $this->expectExceptionMessage('The name field must be a string.');
+        $this->expectExceptionCode(422);
+
+        $this->stringMethod->execute(['name' => 123]);
+    }
+}
