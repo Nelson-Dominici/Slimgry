9a6742a8d7383255ccbc88d75063dbcd38fec7c8
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-02-05 14:47:24 -0300

    Removing the return from the "assertAndThrow" method

--- src/ValidationMethod/Methods/StringMethod.php
+++ src/ValidationMethod/Methods/StringMethod.php
@@ -8,14 +8,15 @@ class StringMethod extends ValidationMethod
 {
     public function execute(array $requestBody): null
     {    
-        if (!array_key_exists($this->fieldToValidate, $requestBody)) {
-            return null;
+        if (array_key_exists($this->fieldToValidate, $requestBody)) {
+
+            $exceptionMessage = "The {$this->fieldToValidate} field must be a string.";
+
+            $isString = is_string($requestBody[$this->fieldToValidate]);
+    
+            $this->assertAndThrow(!$isString, $exceptionMessage);
         }
 
-        $exceptionMessage = "The {$this->fieldToValidate} field must be a string.";
-
-        $isString = is_string($requestBody[$this->fieldToValidate]);
-
-        return $this->assertAndThrow(!$isString, $exceptionMessage);
+        return null;
     }
 }
